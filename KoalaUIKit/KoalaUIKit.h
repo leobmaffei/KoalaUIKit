e257228710ca06d1fcd4148bf1e0a25df5e56aa1
Leonardo Maffei <leobmaffei@gmail.com>
2021-10-05 18:01:25 -0300

    Initial commit

---
+++ KoalaUIKit/KoalaUIKit.h
@@ -0,0 +1,18 @@
+//
+//  KoalaUIKit.h
+//  KoalaUIKit
+//
+//  Created by Leonardo Maffei on 05/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KoalaUIKit.
+FOUNDATION_EXPORT double KoalaUIKitVersionNumber;
+
+//! Project version string for KoalaUIKit.
+FOUNDATION_EXPORT const unsigned char KoalaUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KoalaUIKit/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# KoalaUIKit
+ 
